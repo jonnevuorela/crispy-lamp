83acc56add9f97f3ec51623979c566e9093f12bf
Jonne Vuorela <vuorelajonne@gmail.com>
2024-02-11 17:02:55 +0200

    ei toimi

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-<<<<<<< HEAD
-    /*plugins: ["expo-router/babel"],*/
-=======
-    plugins: ["expo-router/babel"],
->>>>>>> ea391de81ed3ba0f136d08f9384d089cb8dfd7f7
+    presets: ['babel-preset-expo'],
+    plugins: ['@babel/plugin-proposal-unicode-property-regex'],
   };
 };
