d382d5845aa258d765a3e157d0416e510f77ffb2
Jonne Vuorela <vuorelajonne@gmail.com>
2024-02-12 13:38:36 +0200

    toimii, UI:ta kehitetty

--- app/index.js
+++ app/index.js
@@ -1,68 +1,44 @@
-import React from "react";
-import { useState } from "react";
-import { View, ScrollView, SafeAreaView } from "react-native";
-import { Stack, useRouter } from "expo-router";
-import { NavigationContainer } from '@react-navigation/native';
-import Home from "./Home";
+import React from "react"
+import {useState}   from "react";
+import { View, ScrollView, SafeAreaView} from "react-native";
+import {Stack, useRouter} from "expo-router";
 
-
-import { COLORS, icons, images, SIZES } from "../constants";
+import {COLORS, icons, images, SIZES} from "../constants";
 import {
-    Nearbyjobs,
-    Popularjobs,
-    ScreenHeaderBtn,
-    Welcome,
+    Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome
 } from "../components";
 
-const HomeScreen = () => {
+
+const Home = () => {
     const router = useRouter();
-
+    
     return (
-    <NavigationContainer>
-        <HomeNavigator />
-    </NavigationContainer>
-    );
-};
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%"/>
+                    ),
+                    headerTitle: ""
+                }}
+            />
 
+            <ScrollView showVerticalScrollIndicator={false}>
+                <View style={{flex: 1,padding: SIZES.medium}}>
+                    <Welcome 
+                    
+                    />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
 
-return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-        <ScrollView showVerticalScrollIndicator={true}>
-            <View
-                style={{
-                    flex: 1,
-                    padding: SIZES.medium,
-            }}
-        >
-            <Welcome />
-            <Popularjobs />
-            <Nearbyjobs />
-        </View>
-    </ScrollView>
-</SafeAreaView>
-);
-
-const HomeNavigator = () => {
-    const router = useRouter();
-
-    return (
-        <Stack.Navigator>
-        <Stack.Screen
-            name="HomeScreen"
-            component={HomeScreen}
-            options={{
-            headerStyle: { backgroundColor: COLORS.lightWhite },
-            headerShadowVisible: false,
-            headerLeft: () => (
-                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-            ),
-            headerRight: () => (
-                <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
-            ),
-            headerTitle: "home",
-            }}
-        />
-        </Stack.Navigator>
-    );
-};
-export const Home;
+export default Home;
