a9cf8a8d4e42fcfd95c6a1c471700112eeec792e
Jonne Vuorela <vuorelajonne@gmail.com>
2024-02-13 13:17:32 +0200

    API toteutettu

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -6,19 +6,30 @@ import {useRouter} from "expo-router"
 import styles from './popularjobs.style'
 import {COLORS,SIZES} from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
     const router = useRouter();
-    const isLoading =false;
-    const error = false;
+
+    const {data,isLoading,error}=useFetch
+    ("search",{
+      query: "React developer",
+      num_pages:1
+    })
+    console.log(data);
+
   return (
     <View style={styles.container}>
+
       <View style ={styles.header}>
         <Text style={styles.headerTitle}>Popular jobs</Text>
+
         <Pressable>
           <Text style={styles.headerBtn}>Show all</Text>
         </Pressable>
+
       </View>
+
       <View style={styles.cardsContainer}>
         {isLoading ? (
           <ActivityIndicator size="large" color={COLORS.primary} />
@@ -26,11 +37,14 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ): (
           <FlatList
+
             data={[1,2,3,4]}
             renderItem  ={({item})=>(
+
               <PopularJobCard
                 item={item}
               />
+
             )}
             keyExtractor={item=>item?.job_id}
             contentContainerStyle={{columnGap: SIZES.medium}}
---
+++ hook/useFetch.js
@@ -0,0 +1,52 @@
+import {useState, useEffect} from 'react';
+import axios from "axios";
+
+
+const useFetch = (endpoint,query)=>{
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error,setError]=useState(null);
+
+    const axios = require('axios');
+
+    const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+        'X-RapidAPI-Key': 'b01f874d7fmsh267b128c5293bdbp1cf541jsn3ce5bfba2fbd',
+        'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    },
+    params: {...query},
+    };
+
+
+    const fetchData=async ()=>{
+        setIsLoading(true);
+
+        try{
+            const response=await axios.request
+            (options);
+
+            setData(response.data.data);
+            setIsLoading(false);
+        }catch(error){
+            setError(error);
+            alert("There is an error")
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const refetch=()=>{
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return {data,isLoading,error,refetch};
+}
+
+export default useFetch
